e97bf4f7cf4dca8ac1bf848fe66d82c41d7f77e9
KCkingcollin <kckingcollin99@gmail.com>
2024-06-08 04:51:17 -0400

    Added -S option for sudo

--- Install.sh
+++ Install.sh
@@ -3,7 +3,7 @@
 if [ $USER = 'root' ]
 then
     pacman -Syyu --noconfirm sudo hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim foot gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search neofetch kdeconnect npm gtk2 gtk3 gtk4
-    echo "You need to run this script as a user with sudo NOT as root"
+    echo "You need to run this script as a sudo user NOT as root"
     echo "Create a new account?"
     read -p "[Y/n]: " answer
     if [ "$(echo "$answer" | grep -o -m 1 "y")" = "y" ]
@@ -18,7 +18,7 @@ then
             echo "%sudo	ALL=(ALL:ALL) ALL" > /etc/sudoers.d/sudo-enable 
         fi
         cd "/home/$accountName"
-        sudo -i -u $accountName git clone https://github.com/KCkingcollin/kcs-reasonable-configs
+        sudo -S -i -u $accountName git clone https://github.com/KCkingcollin/kcs-reasonable-configs
         cd "/home/$accountName/kcs-reasonable-configs"
         su -c "source Install.sh" $accountName
         return
@@ -31,21 +31,21 @@ then
             echo "%sudo	ALL=(ALL:ALL) ALL" > /etc/sudoers.d/sudo-enable 
         fi
         cd "/home/$accountName"
-        sudo -i -u $accountName git clone https://github.com/KCkingcollin/kcs-reasonable-configs
+        sudo -S -i -u $accountName git clone https://github.com/KCkingcollin/kcs-reasonable-configs
         cd "/home/$accountName/kcs-reasonable-configs"
         su $accountName
         su -c "source Install.sh" $accountName
         return
     fi
 else
-    sudo pacman -Syyu --noconfirm sudo hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim foot gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search neofetch kdeconnect npm gtk2 gtk3 gtk4
+    sudo -S pacman -Syyu --noconfirm sudo hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim foot gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search neofetch kdeconnect npm gtk2 gtk3 gtk4
 fi
 
 if [ "$(pacman -Q | grep -o -m 1 yay)" != "yay" ];
 then
     if [ "$(ls | grep -o -m 1 "yay")" = "yay" ];
     then 
-        sudo rm -r ./yay/
+        sudo -S rm -r ./yay/
     fi
     git clone https://aur.archlinux.org/yay.git
     cd yay
@@ -55,19 +55,19 @@ fi
 
 yay -S --noconfirm hyprshot nvim-packer-git hy3-git oh-my-zsh-git hyprland-git hyprland-plugin-hyprbars-git nwg-shell
 
-flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak -y install org.mozilla.firefox/x86_64/stable
-sudo flatpak override --filesystem="$HOME"/.themes
-sudo flatpak override --filesystem="$HOME"/.icons
-sudo flatpak override --filesystem="$HOME"/.gtkrc-2.0
-sudo flatpak override --env=GTK_THEME=Adwaita-dark
-sudo flatpak override --env=ICON_THEME=Adwaita-dark
+sudo -S flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo -S flatpak -y install org.mozilla.firefox/x86_64/stable
+sudo -S flatpak override --filesystem="$HOME"/.themes
+sudo -S flatpak override --filesystem="$HOME"/.icons
+sudo -S flatpak override --filesystem="$HOME"/.gtkrc-2.0
+sudo -S flatpak override --env=GTK_THEME=Adwaita-dark
+sudo -S flatpak override --env=ICON_THEME=Adwaita-dark
 
 if [ "$(git status | grep -o -m 1 "On branch main")" != "On branch main" ]
 then
     if [ "$(ls | grep -o -m 1 "kcs-reasonable-configs")" = "kcs-reasonable-configs" ];
     then 
-        sudo rm -r ./kcs-reasonable-configs/
+        sudo -S rm -r ./kcs-reasonable-configs/
     fi
     git clone https://github.com/KCkingcollin/kcs-reasonable-configs
     cd kcs-reasonable-configs
@@ -87,17 +87,15 @@ location="$(pwd)"
 
 mkdir $HOME/.config
 
-sudo chmod +x "$location"/switch-DEs.sh
-
 yes | cp -rf "$location"/nvim "$location"/foot "$location"/hypr "$location"/waybar "$location"/swaync "$location"/rofi "$HOME/.config/"
 
 yes | cp -rf "$location"/.zshrc "$location"/.themes "$location"/.icons "$location"/.gtkrc-2.0 "$HOME/"
 
-sudo cp -r "$location"/switch-DEs.sh /usr/bin/
+sudo -S cp -r "$location"/switch-DEs.sh /usr/bin/
 
-sudo cp -r "$location"/switch-DEs.service /etc/systemd/system/
+sudo -S cp -r "$location"/switch-DEs.service /etc/systemd/system/
 
-sudo chsh -s /bin/zsh $USER
+sudo -S chsh -s /bin/zsh $USER
 
 if [ "$(ls "$HOME/Pictures/" | grep -o -m 1 "background.jpg")" != "background.jpg" ];
 then
@@ -107,4 +105,4 @@ fi
 
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
-sudo systemctl start switch-DEs.service
+sudo -S systemctl start switch-DEs.service
