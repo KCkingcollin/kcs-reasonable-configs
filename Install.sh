d20bb270195286e92755e77adeb106b3a1d6e20c
KCkingcollin <kckingcollin99@gmail.com>
2024-06-08 02:15:24 -0400

    Fixed a thing?

--- Install.sh
+++ Install.sh
@@ -13,7 +13,7 @@ then
         passwd $accountName
         groupadd sudo
         usermod -aG sudo $accountName
-        if [ $(cat /etc/sudoers | grep -o -m 1 "# %sudo") = "# %sudo" ]
+        if [ "$(cat /etc/sudoers | grep -o -m 1 "# %sudo")" = "# %sudo" ]
         then
             echo "%sudo	ALL=(ALL:ALL) ALL" > /etc/sudoers.d/sudo-enable 
         fi
