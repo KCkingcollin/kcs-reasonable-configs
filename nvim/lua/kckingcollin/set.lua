467b73ef90b1cd8fbf494b148683242867cd153c
KCkingcollin <kckingcollin99@gmail.com>
2024-06-13 20:39:25 -0400

    Fixed line breaking

--- nvim/lua/kckingcollin/set.lua
+++ nvim/lua/kckingcollin/set.lua
@@ -1,20 +1,21 @@
 -- Function to enable wrap if buffer is empty (new file)
 local function enable_wrap_if_empty()
-  if vim.fn.line2byte('$') == -1 then
-    vim.wo.wrap = true
-  end
+    if vim.fn.line2byte('$') == -1 then
+        vim.wo.wrap = true
+        vim.wo.linebreak = true
+    end
 end
 
 -- Helper function to check if wrap should be enabled based on filetype
 local function should_enable_wrap()
-  local filetype = vim.bo.filetype
-  local wrap_filetypes = { "markdown", "text", "txt", "md" } -- Add your new filetype here
-  for _, ft in ipairs(wrap_filetypes) do
-    if filetype == ft then
-      return true
+    local filetype = vim.bo.filetype
+    local wrap_filetypes = { "markdown", "text", "txt", "md" } -- Add your new filetype here
+    for _, ft in ipairs(wrap_filetypes) do
+        if filetype == ft then
+            return true
+        end
     end
-  end
-  return false
+    return false
 end
 
 vim.opt.guicursor = ""
@@ -58,47 +59,50 @@ vim.opt.clipboard = "unnamedplus"
 
 -- Autocommands for specific file types
 vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
-  pattern = {'*.txt', '*.md'}, -- Add your new file extension here
-  callback = function()
-    vim.wo.wrap = true
-  end,
+    pattern = {'*.txt', '*.md'}, -- Add your new file extension here
+    callback = function()
+        vim.wo.wrap = true
+        vim.wo.linebreak = true
+    end,
 })
 
 -- Autocommand for startup with an empty buffer
 vim.api.nvim_create_autocmd('BufWinEnter', {
-  callback = function()
-    enable_wrap_if_empty()
-  end,
+    callback = function()
+        enable_wrap_if_empty()
+    end,
 })
 
 -- Autocommand for when a new buffer is created
 vim.api.nvim_create_autocmd('BufNewFile', {
-  callback = function()
-    enable_wrap_if_empty()
-  end,
+    callback = function()
+        enable_wrap_if_empty()
+    end,
 })
 
 -- Autocommand for buffer enter
 vim.api.nvim_create_autocmd('BufEnter', {
-  callback = function()
-    if vim.bo.filetype == "" then
-      enable_wrap_if_empty()
-    elseif should_enable_wrap() then
-      vim.wo.wrap = true
-    else
-      vim.wo.wrap = false
-    end
-  end,
+    callback = function()
+        if vim.bo.filetype == "" then
+            enable_wrap_if_empty()
+        elseif should_enable_wrap() then
+            vim.wo.wrap = true
+            vim.wo.linebreak = true
+        else
+            vim.wo.wrap = false
+            vim.wo.linebreak = false
+        end
+    end,
 })
 
 -- Autocommand for buffer read post
 vim.api.nvim_create_autocmd('BufReadPost', {
-  pattern = '*',
-  callback = function()
-    if should_enable_wrap() then
-      vim.wo.wrap = true
-    else
-      vim.wo.wrap = false
-    end
-  end,
+    pattern = '*',
+    callback = function()
+        if should_enable_wrap() then
+            vim.wo.wrap = true
+        else
+            vim.wo.wrap = false
+        end
+    end,
 })
