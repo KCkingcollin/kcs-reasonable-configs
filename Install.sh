7e517a95c91bdd227d5a09d0dfccb481d30ece83
KCkingcollin <kckingcollin99@gmail.com>
2024-06-18 13:59:32 -0400

    Adjusted install script to use castle shell repo for color-checker

--- Install.sh
+++ Install.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
-if [ $USER = 'root' ]
+if [ "$USER" = 'root' ]
 then
-    pacman -Syyu --noconfirm sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search neofetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox
+    pacman -Syyu --noconfirm sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search neofetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox go
     echo "You need to run this script as a sudo user NOT as root"
     echo "Create a new account?"
-    read -p "[Y/n]: " answer
+    read -rp "[Y/n]: " answer
     if [ "$(echo "$answer" | grep -o -m 1 "y")" = "y" ]
     then
-        read -p "Name of the account?: " accountName
-        useradd -m $accountName
-        passwd $accountName
+        read -rp "Name of the account?: " accountName
+        useradd -m "$accountName"
+        passwd "$accountName"
         groupadd sudo
-        usermod -aG sudo $accountName
-        if [ "$(cat /etc/sudoers | grep -o -m 1 "# %sudo")" = "# %sudo" ]
+        usermod -aG sudo "$accountName"
+        if [ "$(grep -o -m 1 "# %sudo" < /etc/sudoers)" = "# %sudo" ]
         then
             echo "%sudo	ALL=(ALL:ALL) ALL" > /etc/sudoers.d/sudo-enable 
         fi
-        cd "/home/$accountName"
-        sudo -S -i -u $accountName git clone https://github.com/KCkingcollin/kcs-reasonable-configs
-        cd "/home/$accountName/kcs-reasonable-configs"
-        su -c "./Install.sh" $accountName
+        cd "/home/$accountName" || return
+        sudo -S -i -u "$accountName" git clone https://github.com/KCkingcollin/kcs-reasonable-configs
+        cd "/home/$accountName/kcs-reasonable-configs" || return
+        su -c "./Install.sh" "$accountName"
         return
     else
-        read -p "Username?: " accountName
+        read -rp "Username?: " accountName
         groupadd sudo
-        usermod -aG sudo $accountName
-        if [ "$(cat /etc/sudoers | grep -o -m 1 "# %sudo")" = "# %sudo" ]
+        usermod -aG sudo "$accountName"
+        if [ "$(grep -o -m 1 "# %sudo" < /etc/sudoers)" = "# %sudo" ]
         then
             echo "%sudo	ALL=(ALL:ALL) ALL" > /etc/sudoers.d/sudo-enable 
         fi
-        cd "/home/$accountName"
-        sudo -S -i -u $accountName git clone https://github.com/KCkingcollin/kcs-reasonable-configs
-        cd "/home/$accountName/kcs-reasonable-configs"
-        su $accountName
-        su -c "./Install.sh" $accountName
+        cd "/home/$accountName" || return
+        sudo -S -i -u "$accountName" git clone https://github.com/KCkingcollin/kcs-reasonable-configs
+        cd "/home/$accountName/kcs-reasonable-configs" || return
+        su -c "./Install.sh" "$accountName"
         return
     fi
 else
-    sudo -S pacman -Syyu --noconfirm sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search neofetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox
+    sudo -S pacman -Syyu --noconfirm sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search neofetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox go
 fi
 
 if [ "$(pacman -Q | grep -o -m 1 yay)" != "yay" ];
@@ -48,12 +47,21 @@ then
         sudo -S rm -r ./yay/
     fi
     git clone https://aur.archlinux.org/yay.git
-    cd yay
+    cd yay || return
     makepkg -si --noconfirm
     cd ..
 fi
 
 yay -S --noconfirm hyprshot nvim-packer-git oh-my-zsh-git nwg-displays pamac-all
+
+if [ "$(ls | grep -o -m 1 "castle-shell")" = "castle-shell" ];
+then 
+    sudo -S rm -r ./castle-shell/
+fi
+git clone https://github.com/KCkingcollin/castle-shell
+cd castle-shell/color-checker || return
+sudo -S go build -o /usr/bin/color-checker
+cd ../..
 
 sudo -S flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo -S flatpak override --filesystem="$HOME"/.themes
@@ -68,8 +76,8 @@ then
     then 
         sudo -S rm -r ./kcs-reasonable-configs/
     fi
-    git clone https://github.com/KCkingcollin/kcs-reasonable-configs
-    cd kcs-reasonable-configs
+    git clone https://github.com/KCkingcollin/kcs-reasonable-config
+    cd kcs-reasonable-configs || return
 fi
 mv "$HOME/.config/nvim" "$HOME/.config/nvim.bac" 
 mv "$HOME/.config/kitty" "$HOME/.config/foot.bac" 
@@ -83,15 +91,13 @@ mv "$HOME/.themes" "$HOME/.themes.bac"
 mv "$HOME/.icons" "$HOME/.icons.bac" 
 mv "$HOME/.gtkrc-2.0" "$HOME/.gtkrc-2.0.bac" 
 
-mkdir $HOME/.config
+mkdir "$HOME"/.config
 
 yes | cp -rf ./nvim ./kitty ./hypr ./waybar ./swaync ./rofi ./castle-shell "$HOME/.config/"
 
 yes | cp -rf ./.zshrc ./.themes ./.icons ./.gtkrc-2.0 "$HOME/"
 
-sudo -S cp -rf ./switch-DEs.sh ./color-checker.py /usr/bin/
-sudo -S mv /usr/bin/color-checker.py /usr/bin/color-checker
-sudo -S mv /usr/bin/switch-DEs.sh /usr/bin/switch-DEs
+sudo -S cp -rf ./switch-DEs.sh /usr/bin/switch-DEs
 
 sudo -S cp -rf ./theme-check.service ./waybar-hyprland.service /usr/lib/systemd/user/
 
@@ -120,6 +126,6 @@ sudo -S bash -c 'echo "[User]
 Session=hyprland
 XSession=hyprland
 Icon="$HOME"/.face
-SystemAccount=false" > /var/lib/AccountsService/users/'$USER''
+SystemAccount=false" > /var/lib/AccountsService/users/"$USER"'
 
 sudo -S systemctl start switch-DEs.service
