bd535fadafbc1d7132cf34060851544172fc010a
KCkingcollin <kckingcollin99@gmail.com>
2025-07-09 19:46:51 -0400

    Added pacman config and mirrorlist, did a lot of code cleanup, and fixed some bugs

--- Install.sh
+++ Install.sh
@@ -16,81 +16,107 @@ function chrootInstall {
     echo "%sudo	ALL=(ALL:ALL) ALL" > /etc/sudoers.d/sudo-enable 
     echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" > /etc/pacman.conf
     cd "/home/$accountName" || return
-
-    sudo -S -i -u "$accountName" "$(
-        git clone https://aur.archlinux.org/yay.git
-        cd yay || return
-        makepkg -si --noconfirm
-        cd ..
-        yay -S --noconfirm hyprshot nvim-packer-git oh-my-zsh-git nwg-displays pamac-all
-
-        git clone https://github.com/KCkingcollin/kcs-reasonable-configs
-        cd "kcs-reasonable-configs" || return
-
-        git clone https://github.com/KCkingcollin/castle-shell
-        cd castle-shell/color-checker || return
-        sudo -S go build -o /usr/bin/color-checker
-        cd ../..
-
-        sudo -S flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
-        sudo -S flatpak override --filesystem="$accountName"/.themes
-        sudo -S flatpak override --filesystem="$accountName"/.icons
-        sudo -S flatpak override --filesystem="$accountName"/.gtkrc-2.0
-        sudo -S flatpak override --env=GTK_THEME=Adwaita-dark
-        sudo -S flatpak override --env=ICON_THEME=Adwaita-dark
-
-        mv "$accountName/.config/nvim" "$accountName/.config/nvim.bac" 
-        mv "$accountName/.config/fastfetch" "$accountName/.config/fastfetch.bac" 
-        mv "$accountName/.config/kitty" "$accountName/.config/foot.bac" 
-        mv "$accountName/.config/hypr" "$accountName/.config/hypr.bac" 
-        mv "$accountName/.config/waybar" "$accountName/.config/waybar.bac" 
-        mv "$accountName/.config/swaync" "$accountName/.config/swaync.bac" 
-        mv "$accountName/.config/rofi" "$accountName/.config/rofi.bac" 
-        mv "$accountName/.config/castle-shell" "$accountName/.config/castle-shell.bac" 
-        mv "$accountName/.zshrc" "$accountName/.zshrc.bac" 
-        mv "$accountName/.themes" "$accountName/.themes.bac" 
-        mv "$accountName/.icons" "$accountName/.icons.bac" 
-        mv "$accountName/.gtkrc-2.0" "$accountName/.gtkrc-2.0.bac" 
-
-        mkdir "$accountName"/.config
-
-        yes | cp -rf ./nvim ./kitty ./hypr ./waybar ./swaync ./rofi ./castle-shell ./fastfetch "$accountName/.config/"
-
-        yes | cp -rf ./.zshrc ./.themes ./.icons ./.gtkrc-2.0 "$accountName/"
-
-        sudo -S cp -rf ./switch-DEs.sh /usr/bin/switch-DEs
-
-        sudo -S cp -rf ./theme-check.service ./waybar-hyprland.service /usr/lib/systemd/user/
-
-        sudo -S cp -rf ./switch-DEs.service  /etc/systemd/system/
-
-        yes | cp -rf ./after.sh /"$accountName"/.config/hypr/
-
-        mv /"$accountName"/.config/hypr/hyprland.conf /"$accountName"/.config/hypr/hyprland.conf.bac
-
-        yes | cp -rf ./hyprland.conf.once /"$accountName"/.config/hypr/hyprland.conf
-
-        sudo -S chsh -s /bin/zsh "$USER"
-
-        mkdir -p "$accountName/Pictures" 
-        cp ./background.jpg "$accountName/Pictures/background.jpg"
-
-        nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
-        # make damn sure it gets the environment before running hyprland the first time
-        systemctl --user import-environment
-    )"
-
-    echo "[User]
-Session=hyprland
-XSession=hyprland
-Icon=$accountName/.face
-SystemAccount=false" > /var/lib/AccountsService/users/"$accountName"
+    sudo -S -i -u "$accountName" "$(configSetup "$accountName")"
     echo "Done"
     echo "Reboot into the new drive"
 }
 
+function configSetup {
+    userName=$1
+    if [ "$(pacman -Q | grep -o -m 1 yay)" != "yay" ];
+    then
+        if [ "$(ls | grep -o -m 1 "yay")" = "yay" ];
+        then 
+            sudo -S rm -r ./yay/
+        fi
+        git clone https://aur.archlinux.org/yay.git
+        cd yay || return
+        makepkg -si --noconfirm
+        cd ..
+    fi
+
+    yay -S --noconfirm hyprshot nvim-packer-git oh-my-zsh-git nwg-displays pamac-all
+
+    if [ "$(ls | grep -o -m 1 "castle-shell")" = "castle-shell" ];
+    then 
+        sudo -S rm -r ./castle-shell/
+    fi
+    git clone https://github.com/KCkingcollin/castle-shell
+    cd castle-shell/color-checker || return
+    sudo -S go build -o /usr/bin/color-checker
+    cd ../..
+
+    sudo -S flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
+    sudo -S flatpak override --filesystem="$userName"/.themes
+    sudo -S flatpak override --filesystem="$userName"/.icons
+    sudo -S flatpak override --filesystem="$userName"/.gtkrc-2.0
+    sudo -S flatpak override --env=GTK_THEME=Adwaita-dark
+    sudo -S flatpak override --env=ICON_THEME=Adwaita-dark
+
+    if [ "$(git status | grep -o -m 1 "On branch main")" != "On branch main" ]
+    then
+        if [ "$(ls | grep -o -m 1 "kcs-reasonable-configs")" = "kcs-reasonable-configs" ];
+        then 
+            sudo -S rm -r ./kcs-reasonable-configs/
+        fi
+        git clone https://github.com/KCkingcollin/kcs-reasonable-config
+        cd kcs-reasonable-configs || return
+    fi
+    mv "$userName/.config/nvim" "$userName/.config/nvim.bac" 
+    mv "$userName/.config/fastfetch" "$userName/.config/fastfetch.bac" 
+    mv "$userName/.config/kitty" "$userName/.config/foot.bac" 
+    mv "$userName/.config/hypr" "$userName/.config/hypr.bac" 
+    mv "$userName/.config/waybar" "$userName/.config/waybar.bac" 
+    mv "$userName/.config/swaync" "$userName/.config/swaync.bac" 
+    mv "$userName/.config/rofi" "$userName/.config/rofi.bac" 
+    mv "$userName/.config/castle-shell" "$userName/.config/castle-shell.bac" 
+    mv "$userName/.zshrc" "$userName/.zshrc.bac" 
+    mv "$userName/.themes" "$userName/.themes.bac" 
+    mv "$userName/.icons" "$userName/.icons.bac" 
+    mv "$userName/.gtkrc-2.0" "$userName/.gtkrc-2.0.bac" 
+
+    mkdir "$userName"/.config
+
+    yes | cp -rf ./nvim ./kitty ./hypr ./waybar ./swaync ./rofi ./castle-shell ./fastfetch "$userName/.config/"
+
+    yes | cp -rf ./.zshrc ./.themes ./.icons ./.gtkrc-2.0 "$userName/"
+
+    yes | cp -rf pacman* /etc/
+
+    sudo -S cp -rf ./switch-DEs.sh /usr/bin/switch-DEs
+
+    sudo -S cp -rf ./theme-check.service ./waybar-hyprland.service /usr/lib/systemd/user/
+
+    sudo -S cp -rf ./switch-DEs.service  /etc/systemd/system/
+
+    yes | cp -rf ./after.sh /"$userName"/.config/hypr/
+
+    mv /"$userName"/.config/hypr/hyprland.conf /"$userName"/.config/hypr/hyprland.conf.bac
+
+    yes | cp -rf ./hyprland.conf.once /"$userName"/.config/hypr/hyprland.conf
+
+    sudo -S chsh -s /bin/zsh "$userName"
+
+    if [ "$(ls "$userName/Pictures/" | grep -o -m 1 "background.jpg")" != "background.jpg" ];
+    then
+        mkdir -p "$userName/Pictures" 
+        cp ./background.jpg "$userName/Pictures/background.jpg"
+    fi
+
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+    # make damn sure it gets the environment before running hyprland the first time
+    systemctl --user import-environment
+
+    sudo -S bash -c echo '[User]                        
+Session=hyprland
+XSession=hyprland
+Icon="$userName"/.face
+SystemAccount=false' > /var/lib/AccountsService/users/"$userName"
+}
+
 export -f chrootInstall
+export -f configSetup
 
 if [ "$USER" = 'root' ]
 then
@@ -99,6 +125,11 @@ then
     if [ "$(echo "$answer" | grep -o -m 1 "y")" = "y" ] then
         echo "Install dir?"
         read -rp " > " answer
+        (
+            git clone https://github.com/KCkingcollin/kcs-reasonable-config
+            cd kcs-reasonable-configs || return
+            yes | cp -rf pacman* /etc/
+        )
         pacstrap -K "$answer" linux-lts arch-install-scripts os-prober efibootmgr linux-firmware linux-lts-headers grub sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search fastfetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox go wget
         arch-chroot "$answer" /bin/bash -c chrootInstall
         return
@@ -107,7 +138,7 @@ then
     read -rp "[Y/n]: " answer
     if [ "$(echo "$answer" | grep -o -m 1 "y")" = "y" ]
     then
-        chrootInstall
+        "$(chrootInstall)"
         return
     else
         echo "Create a new account?"
@@ -156,93 +187,6 @@ else
     sudo -S pacman -Syyu --noconfirm sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search fastfetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox go
 fi
 
-if [ "$(pacman -Q | grep -o -m 1 yay)" != "yay" ];
-then
-    if [ "$(ls | grep -o -m 1 "yay")" = "yay" ];
-    then 
-        sudo -S rm -r ./yay/
-    fi
-    git clone https://aur.archlinux.org/yay.git
-    cd yay || return
-    makepkg -si --noconfirm
-    cd ..
-fi
-
-yay -S --noconfirm hyprshot nvim-packer-git oh-my-zsh-git nwg-displays pamac-all
-
-if [ "$(ls | grep -o -m 1 "castle-shell")" = "castle-shell" ];
-then 
-    sudo -S rm -r ./castle-shell/
-fi
-git clone https://github.com/KCkingcollin/castle-shell
-cd castle-shell/color-checker || return
-sudo -S go build -o /usr/bin/color-checker
-cd ../..
-
-sudo -S flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo -S flatpak override --filesystem="$HOME"/.themes
-sudo -S flatpak override --filesystem="$HOME"/.icons
-sudo -S flatpak override --filesystem="$HOME"/.gtkrc-2.0
-sudo -S flatpak override --env=GTK_THEME=Adwaita-dark
-sudo -S flatpak override --env=ICON_THEME=Adwaita-dark
-
-if [ "$(git status | grep -o -m 1 "On branch main")" != "On branch main" ]
-then
-    if [ "$(ls | grep -o -m 1 "kcs-reasonable-configs")" = "kcs-reasonable-configs" ];
-    then 
-        sudo -S rm -r ./kcs-reasonable-configs/
-    fi
-    git clone https://github.com/KCkingcollin/kcs-reasonable-config
-    cd kcs-reasonable-configs || return
-fi
-mv "$HOME/.config/nvim" "$HOME/.config/nvim.bac" 
-mv "$HOME/.config/fastfetch" "$HOME/.config/fastfetch.bac" 
-mv "$HOME/.config/kitty" "$HOME/.config/foot.bac" 
-mv "$HOME/.config/hypr" "$HOME/.config/hypr.bac" 
-mv "$HOME/.config/waybar" "$HOME/.config/waybar.bac" 
-mv "$HOME/.config/swaync" "$HOME/.config/swaync.bac" 
-mv "$HOME/.config/rofi" "$HOME/.config/rofi.bac" 
-mv "$HOME/.config/castle-shell" "$HOME/.config/castle-shell.bac" 
-mv "$HOME/.zshrc" "$HOME/.zshrc.bac" 
-mv "$HOME/.themes" "$HOME/.themes.bac" 
-mv "$HOME/.icons" "$HOME/.icons.bac" 
-mv "$HOME/.gtkrc-2.0" "$HOME/.gtkrc-2.0.bac" 
-
-mkdir "$HOME"/.config
-
-yes | cp -rf ./nvim ./kitty ./hypr ./waybar ./swaync ./rofi ./castle-shell ./fastfetch "$HOME/.config/"
-
-yes | cp -rf ./.zshrc ./.themes ./.icons ./.gtkrc-2.0 "$HOME/"
-
-sudo -S cp -rf ./switch-DEs.sh /usr/bin/switch-DEs
-
-sudo -S cp -rf ./theme-check.service ./waybar-hyprland.service /usr/lib/systemd/user/
-
-sudo -S cp -rf ./switch-DEs.service  /etc/systemd/system/
-
-yes | cp -rf ./after.sh /"$HOME"/.config/hypr/
-
-mv /"$HOME"/.config/hypr/hyprland.conf /"$HOME"/.config/hypr/hyprland.conf.bac
-
-yes | cp -rf ./hyprland.conf.once /"$HOME"/.config/hypr/hyprland.conf
-
-sudo -S chsh -s /bin/zsh "$USER"
-
-if [ "$(ls "$HOME/Pictures/" | grep -o -m 1 "background.jpg")" != "background.jpg" ];
-then
-    mkdir -p "$HOME/Pictures" 
-    cp ./background.jpg "$HOME/Pictures/background.jpg"
-fi
-
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
-# make damn sure it gets the environment before running hyprland the first time
-systemctl --user import-environment
-
-sudo -S bash -c 'echo "[User]                        
-Session=hyprland
-XSession=hyprland
-Icon="$HOME"/.face
-SystemAccount=false" > /var/lib/AccountsService/users/"$USER"'
+"$(configSetup "$USER")"
 
 sudo -S systemctl start switch-DEs.service
--- RepairRoot.sh
+++ RepairRoot.sh
@@ -7,37 +7,47 @@ function chrootInstall {
     grub-mkconfig -o /boot/grub/grub.cfg
 
     read -rp "Name of the account?: " accountName
-    sudo -S -i -u "$accountName" "$(
-        git clone https://aur.archlinux.org/yay.git
-        cd yay || return
-        makepkg -si --noconfirm
-        cd ..
-        yay -S --noconfirm hyprshot nvim-packer-git oh-my-zsh-git nwg-displays pamac-all
-
-        git clone https://github.com/KCkingcollin/castle-shell
-        cd castle-shell/color-checker || return
-        sudo -S go build -o /usr/bin/color-checker
-        cd ../..
-
-        sudo -S flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
-        sudo -S flatpak override --filesystem="$accountName"/.themes
-        sudo -S flatpak override --filesystem="$accountName"/.icons
-        sudo -S flatpak override --filesystem="$accountName"/.gtkrc-2.0
-        sudo -S flatpak override --env=GTK_THEME=Adwaita-dark
-        sudo -S flatpak override --env=ICON_THEME=Adwaita-dark
-
-        # make damn sure it gets the environment before running hyprland the first time
-        systemctl --user import-environment
-    )"
+    sudo -S -i -u "$accountName" "$(configSetup "$accountName")"
     echo "Done"
+    echo "In chroot, done fixing"
+}
+
+function configSetup {
+    userName=$1
+    git clone https://aur.archlinux.org/yay.git
+    cd yay || return
+    makepkg -si --noconfirm
+    cd ..
+    yay -S --noconfirm hyprshot nvim-packer-git oh-my-zsh-git nwg-displays pamac-all
+
+    git clone https://github.com/KCkingcollin/castle-shell
+    cd castle-shell/color-checker || return
+    sudo -S go build -o /usr/bin/color-checker
+    cd ../..
+
+    sudo -S flatpak -y remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
+    sudo -S flatpak override --filesystem="$userName"/.themes
+    sudo -S flatpak override --filesystem="$userName"/.icons
+    sudo -S flatpak override --filesystem="$userName"/.gtkrc-2.0
+    sudo -S flatpak override --env=GTK_THEME=Adwaita-dark
+    sudo -S flatpak override --env=ICON_THEME=Adwaita-dark
+
+    # make damn sure it gets the environment before running hyprland the first time
+    systemctl --user import-environment
 }
 
 export -f chrootInstall
+export -f configSetup
 
 if [ "$USER" = 'root' ]; then
-    # checking to make sure we are not in chroot
+    (
+        git clone https://github.com/KCkingcollin/kcs-reasonable-config
+        cd kcs-reasonable-configs || return
+        yes | cp -rf pacman* /etc/
+    )
     echo "root dir?"
     read -rp " > " rootdir
+    # checking to make sure we are not in chroot
     if ! [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
         mkdir oldfiles
         mkdir oldfiles/etc
@@ -51,21 +61,23 @@ if [ "$USER" = 'root' ]; then
         read -rp "[Y/n]: " answer
         if [ "$(echo "$answer" | grep -o -m 1 "y")" = "y" ] then
             for file in *; do
-                if [[ "$file" != *"home"* || "$file" != *"root"* || "$file" != *"dev"* ]]; then 
+                if [[ "$file" != *"home"* || "$file" != *"root"* || "$file" != *"dev"* || "$file" != *"boot"* ]]; then 
                     rm -r "$file"
                 fi
             done
+            rm -r boot/*
         fi
-        cd ../oldfiles || return
-        if ! cp -R etc ../"$rootdir"/; then
+        cd ..
+        pacstrap -K "$rootdir" linux-lts arch-install-scripts os-prober efibootmgr linux-firmware linux-lts-headers grub sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search fastfetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox go wget
+        cd oldfiles || return
+        if ! cp -Rf etc ../"$rootdir"/; then
             echo "couldn't copy some files, not attempting to install"
             return
         fi
         cd ..
-        pacstrap -K "$rootdir" linux-lts arch-install-scripts os-prober efibootmgr linux-firmware linux-lts-headers grub sudo hyprland hyprpaper waybar swaync playerctl polkit-gnome gnome-keyring pipewire wireplumber xdg-desktop-portal-hyprland otf-geist-mono-nerd otf-font-awesome pavucontrol nm-connection-editor networkmanager blueman git base-devel flatpak nemo rofi-wayland neovim kitty gdm cpio meson cmake zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search fastfetch kdeconnect npm gtk2 gtk3 gtk4 hyprwayland-scanner gnome-control-center python xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs firefox go wget
         arch-chroot "$rootdir" /bin/bash -c chrootInstall
     else
-        chrootInstall
+        "$(chrootInstall)"
     fi
 else
     echo "not root"
