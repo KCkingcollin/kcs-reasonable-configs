ce09b30156181f98141ef2d1cd58e3b72efd1be2
KCkingcollin <kckingcollin99@gmail.com>
2025-07-11 06:54:21 -0400

    Added rate-mirrors and a crontab

--- Install.sh
+++ Install.sh
@@ -49,6 +49,7 @@ function chrootSetup {
     passwd
     systemctl enable NetworkManager
     systemctl enable gdm
+    systemctl enable cronie
     pacman -Syyu --noconfirm
     grub-install --target=x86_64-efi --efi-directory=boot/efi --bootloader-id=GRUB
     grub-mkconfig -o /boot/grub/grub.cfg
