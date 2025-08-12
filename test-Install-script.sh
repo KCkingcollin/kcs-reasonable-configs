b72df03378b5d7f431564d706bf181a7c2501bba
KCkingcollin <kckingcollin99@gmail.com>
2025-08-11 22:32:42 -0400

    Automating test VM setup
    Added XML config and some scripting to enable the auto creation and
    deployment of a test environment for the install script

--- test-Install-script.sh
+++ test-Install-script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-if [[ ! -f "arch-intall.qcow2" ]]; then
-  wget -O arch-intall.qcow2 https://gitlab.archlinux.org/archlinux/arch-boxes/-/package_files/10032/download
+archInstallDisk="$HOME/.local/share/libvirt/images/arch-intall.qcow2"
+archTestDisk="$HOME/.local/share/libvirt/images/arch-test.qcow2"
+
+if [[ ! -f "$archInstallDisk" ]]; then
+  wget -O "$archInstallDisk" https://gitlab.archlinux.org/archlinux/arch-boxes/-/package_files/10032/download
 fi
 
-qemu-img create -f qcow2 arch-test.qcow2 30G
+qemu-img create -f qcow2 "$archTestDisk" 30G
 
 sudo modprobe nbd max_part=8
-sudo qemu-nbd -c /dev/nbd0 arch-test.qcow2
+sudo qemu-nbd -c /dev/nbd0 "$archTestDisk"
 
 sudo parted /dev/nbd0 --script \
     mklabel gpt \
@@ -23,3 +26,20 @@ sudo mkfs.ext4 /dev/nbd0p3
 sudo mkswap /dev/nbd0p4
 
 sudo qemu-nbd -d /dev/nbd0
+
+if ! virsh define ./test-arch-vm.xml; then
+    virsh undefine arch-test-vm --nvram
+    virsh define ./test-arch-vm.xml
+fi
+
+virsh attach-disk arch-test-vm "$archInstallDisk" vda --persistent  --subdriver qcow2
+virsh attach-disk arch-test-vm "$archTestDisk" vdb --persistent  --subdriver qcow2
+
+# changes the boot drive via redefining the xml config
+virsh dumpxml arch-test-vm | 
+    sed -e '/<boot order/d' -e \
+    '/<boot dev/d' -e \
+    '/<target dev='\''vda'\''/a\      <boot order='\''1'\''/>' | 
+    virsh define /dev/stdin
+
+virsh start arch-test-vm
